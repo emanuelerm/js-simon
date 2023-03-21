bca1b501dc628fcfde5f7e9cda3f81184fe49da5
emanuelerm <emanueler00@gmail.com>
2023-03-21 19:16:53 +0100

    aggiunta la funzione createInput e startTimeOut

--- js/script.js
+++ js/script.js
@@ -41,30 +41,83 @@ Consigli del giorno:
 
 */
 
+
 const htmlRandomNumbers = document.getElementById("random-numbers");
 const btnGenerateNumbers = document.getElementById("generate-numbers");
 
-//aggiungiamo un eventListener al btn per generare i numeri dopo il click
-btnGenerateNumbers.addEventListener("click", function(){
 
-    //generiamo 5 numeri casuali
-    function getRandomInt(min, max) {
-        //creiamo un array vuoto e attraverso un ciclo while ci assicuriamo che i numeri all'interno non siano più di 5
-        const randomNumbers = [];
-        while (randomNumbers.length < 5) {
-            let number = Math.floor(Math.random() * (max - min + 1)) + min;
-            //tramite il metodo degli array includes, verifichiamo che il numero generato !sia presente nell'array randomNumbers
-            if (!randomNumbers.includes(number)) {
-                //se non c'è lo inseriamo
-                randomNumbers.push(number);
-            }
+//generiamo 5 numeri casuali
+function getRandomInt(min, max) {
+    //creiamo un array vuoto e attraverso un ciclo while ci assicuriamo che i numeri all'interno non siano più di 5
+    const randomNumbers = [];
+    while (randomNumbers.length < 5) {
+        let number = Math.floor(Math.random() * (max - min + 1)) + min;
+        //tramite il metodo degli array includes, verifichiamo che il numero generato !sia presente nell'array randomNumbers
+        if (!randomNumbers.includes(number)) {
+            //se non c'è lo inseriamo
+            randomNumbers.push(number);
         }
-        return randomNumbers;
     }
+    return randomNumbers;
+}
+
+function createInput() {
+    const userNumbers = [];
+    while (userNumbers.length < 5) {
+        let input = document.createElement("input");
+        input.setAttribute("type", "number");
+        input.classList.add("d-block");
+        inputContainer.append(input);
+        userNumbers.push(input);
+    }
+    return (userNumbers, input);
+}
+
+
+function startTimeOut() {
+    let count = 10;
+    const intervalId = setInterval(timeOut, 1000);
+
+    function timeOut() {
+        count--;
+        if (count === 0) {
+            clearInterval(intervalId);
+            htmlRandomNumbers.textContent = "";
+            const title = document.createElement("h2");
+            title.textContent = "Inserisci i numeri che hai memorizzato";
+            htmlRandomNumbers.append(title);
+            //richiamiamo createInput per generare i campi di input dopo che sarà scaduto il timeout
+            createInput();
+        }
+
+    }
+}
+
+function getInputValues(userNumbers) {
+
+
+}
+
+
+
+
+//aggiungiamo un eventListener al btn per generare i numeri dopo il click
+btnGenerateNumbers.addEventListener("click", function () {
 
     //richiamiamo la funzione che genera i numeri e attribuiamo i valori (min, max)
     const randomNumbers = getRandomInt(1, 50);
     //join ci permette di formattare gli elementi dell'array che verranno inseriti all'interno del paragrafo
     htmlRandomNumbers.textContent = randomNumbers.join(" - ");
+    //richiamiamo la funzione startTimeOut per far partire il countdown
+    startTimeOut();
 })
+
+const btnGetInputValues = document.getElementById("get-values");
+
+btnGetInputValues.addEventListener("click", function () {
+
+})
+
+
+
 
