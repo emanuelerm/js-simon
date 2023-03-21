32fb3ddca8e88f7a20e346bbf497bc44b86756bd
emanuelerm <emanueler00@gmail.com>
2023-03-21 15:14:56 +0100

    /*analisi del problema*/

---
+++ js/script.js
@@ -0,0 +1,42 @@
+/*
+Descrizione:
+Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, i numeri che ha visto precedentemente.
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+Consigli del giorno:
+* Pensate prima in italiano.
+* Dividete in piccoli problemi la consegna.
+* Individuate gli elementi di cui avete bisogno per realizzare il programma.
+*/
+
+
+/* Analisi della consegna
+
+//Funzione per generare 5 numeri casuali
+
+
+//Funzione per far partire il timer dopo che sono stati generati tutti i numeri
+    //Far sparire i numeri allo scadere del timer
+
+
+//Dopo step-2, far inserire all'utente i 5 numeri
+    //Indicare quanti e quali numeri sono stati individuati
+
+*/
+
+
+/* Individuazione degli elementi necessari
+
+    - individuare dove andranno disposti i numeri casuali
+    
+    - generare attraverso una funzione i 5 numeri
+        tenere in una variabile il valore di ognuno dei 5 numeri
+    
+        - attraverso la funzione setInterval generare un timer di 30 secondi (30'000ms)
+        scaduto il tempo, rimuovere visivamente i numeri generati in precedenza
+    
+        - far inserire attraverso 5 campi di input i numeri generati all'inizio
+        comparare i dati inseriti dall'utente con i numeri estratti
+            indicare quanti e quali sono corretti
+
+*/
